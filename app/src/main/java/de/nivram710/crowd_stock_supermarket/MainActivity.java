f281545f87fea491fbb4242b5b3c0a436bddae4c
Marvin Jütte <contact@nivram710.de>
2020-03-21 16:58:24 +0100

    access and read location

--- app/src/main/java/de/nivram710/crowd_stock_supermarket/MainActivity.java
+++ app/src/main/java/de/nivram710/crowd_stock_supermarket/MainActivity.java
@@ -1,8 +1,13 @@
 package de.nivram710.crowd_stock_supermarket;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,8 +19,10 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
+    private static final String TAG = "MainActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +39,27 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
 
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, "onLocationChanged: location: " + location.getLatitude() + "; " + location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.i(TAG, "onStatusChanged: gps status changed");
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.i(TAG, "onProviderEnabled: GPS Enabled");
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.i(TAG, "onProviderDisabled: gps disabled");
     }
 }
