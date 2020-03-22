1c213cbb73bd70bfc5c1dd6ca74c8dcae22d2533
Marvin Jütte <contact@nivram710.de>
2020-03-22 11:41:36 +0100

    open detail view if card is clicked
    known issue: products and availability are swapped

--- app/src/main/java/de/nivram710/crowd_stock_supermarket/ui/markets/DetailActivity.java
+++ app/src/main/java/de/nivram710/crowd_stock_supermarket/ui/markets/DetailActivity.java
@@ -1,16 +1,43 @@
 package de.nivram710.crowd_stock_supermarket.ui.markets;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 import de.nivram710.crowd_stock_supermarket.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+        String storeId = extras.getString("id");
+        String name = extras.getString("name");
+        String address = extras.getString("address");
+        boolean isOpen = extras.getBoolean("isOpen");
+
+        TextView textViewStoreName = findViewById(R.id.text_view_store_name);
+        textViewStoreName.setText(name);
+
+        TextView textViewStoreAddress = findViewById(R.id.text_view_address);
+        textViewStoreAddress.setText(address);
+
+        TextView textViewIsOpen = findViewById(R.id.text_view_is_open);
+        if(isOpen) {
+            textViewIsOpen.setText(getString(R.string.store_is_open));
+            textViewIsOpen.setTextColor(getColor(R.color.holoGreenLight));
+        } else {
+            textViewIsOpen.setText(getString(R.string.store_is_closed));
+            textViewIsOpen.setTextColor(getColor(R.color.holoGreenLight));
+        }
+
     }
 }
--- app/src/main/java/de/nivram710/crowd_stock_supermarket/ui/markets/RCCAdapter.java
+++ app/src/main/java/de/nivram710/crowd_stock_supermarket/ui/markets/RCCAdapter.java
@@ -1,6 +1,7 @@
 package de.nivram710.crowd_stock_supermarket.ui.markets;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,10 +42,10 @@ public class RCCAdapter extends BaseAdapter {
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
         view = View.inflate(context, R.layout.store_card, null);
 
-        Store store = stores.get(position);
+        final Store store = stores.get(position);
 
         TextView textViewName = view.findViewById(R.id.text_view_store_name);
         textViewName.setText(store.getName());
@@ -63,7 +64,7 @@ public class RCCAdapter extends BaseAdapter {
         }
 
         TextView indicator_milk = view.findViewById(R.id.color_indicator_milk);
-        TextView indicator_bread = view.findViewById(R.id.color_indicator_bread);
+        final TextView indicator_bread = view.findViewById(R.id.color_indicator_bread);
         TextView indicator_toilet_paper = view.findViewById(R.id.color_indicator_toilet_paper);
         if(store.getProducts().length >= 3) {
             switch (store.getProducts()[0].getAvailability()) {
@@ -103,6 +104,21 @@ public class RCCAdapter extends BaseAdapter {
             indicator_bread.setBackground(context.getDrawable(R.drawable.circle_gray));
             indicator_toilet_paper.setBackground(context.getDrawable(R.drawable.circle_gray));
         }
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Store store = stores.get(position);
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("id", store.getId());
+                intent.putExtra("name", store.getName());
+                intent.putExtra("address", store.getAddress());
+                intent.putExtra("isOpen", store.isOpen());
+
+                context.startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
