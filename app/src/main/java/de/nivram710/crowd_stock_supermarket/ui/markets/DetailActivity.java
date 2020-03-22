0345b3c4747c0809c9b3d21d8dfd41ac2169dd94
Marvin Jütte <contact@nivram710.de>
2020-03-22 10:24:51 +0100

    add first implementation for detail view

---
+++ app/src/main/java/de/nivram710/crowd_stock_supermarket/ui/markets/DetailActivity.java
@@ -0,0 +1,16 @@
+package de.nivram710.crowd_stock_supermarket.ui.markets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.nivram710.crowd_stock_supermarket.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
